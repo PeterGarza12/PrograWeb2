ea1765161370a6674c643ca7935d8be62454d848
FuigerCoba <89951566+FuigerCoba@users.noreply.github.com>
2022-09-21 14:13:22 -0500

    Cambios
    a

--- dtos/products.dto.js
+++ dtos/products.dto.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 //SCHEMA PARA DATOS REQUERIDOS Y LOGICA DE NEGOCIO
-const id = Joi.string().alphanum();
+const id = Joi.string();
 const isActive = Joi.boolean();
 const name = Joi.string().min(3).max(50);
 const price = Joi.number().integer().min(10);
--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const {
   errorHandler,
   boomErrorHandler,
 } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
+
 app.use(
   express.json({
     extended: false, // permite codificar matrices y objetos enriquecidos en formato codificado en url
   })
 );
+
 app.get('/', (req, res) => res.send('Ruta principal'));
+
 routerApi(app);
+
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
