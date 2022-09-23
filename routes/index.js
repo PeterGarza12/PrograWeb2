37c80aab6e11128e8f45f313339c080e390c9cf0
Mundillo <78626148+Mundillo@users.noreply.github.com>
2022-09-22 21:35:48 -0500

    categorias

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const productsRouter = require('./products.router');
+const categoriesRouter = require('./categorias.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
   //router.use('/users', productsRouter);
-  //router.use('/categories', productsRouter);
+ router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
